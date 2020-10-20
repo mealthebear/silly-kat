a7f8f85ee05fcf0b5c84010812d595a22caa1d7e
mealthebear <mealthebear@gmail.com>
2020-10-20 11:59:16 -0700

    Introduced introductory paragraph on home page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,16 +2,27 @@ import React, { useState } from 'react';
 
 const Home = () => {
   return (
-    <section>
-      <div className="space-cat-container">
-        <img 
-          alt="Cat looking into space" 
-          className="space-cat-photo" 
-          src="../../../public/images/space-kat.jpg" 
-        />
-        <p className="space-cat-text">Me be fuzzy cat<br/>looking at the pretty lights</p>
-      </div>
-    </section>
+    <>
+      <section className="generic-section">
+        <div className="space-cat-container">
+          <img 
+            alt="Cat looking into space" 
+            className="space-cat-photo" 
+            src="../../../public/images/space-kat.jpg" 
+          />
+          <p className="space-cat-text">Me be fuzzy cat<br/>looking at the pretty lights</p>
+        </div>
+      </section>
+
+      <section className="generic-section intro-paragraph">
+        <h2 className="intro-title">Who Dat Kat?</h2>
+        <p className="intro-text">Oh hellos! My name is Solly and welcome to my website!
+        I hope you like all the colors and pictures here. I luv luv luv
+        to play Feral Druid and enjoy Mythic Raiding and doing keys ^.^
+        </p>
+      </section>
+
+    </>
   )
 }
 
