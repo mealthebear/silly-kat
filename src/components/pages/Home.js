290c8d468dab2adb38d0922e92be8d3934e711c0
mealthebear <mealthebear@gmail.com>
2020-10-20 11:13:26 -0700

    Displayed text over home page image

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,12 +3,13 @@ import React, { useState } from 'react';
 const Home = () => {
   return (
     <section>
-      <div>
+      <div className="space-cat-container">
         <img 
           alt="Cat looking into space" 
           className="space-cat-photo" 
           src="../../../public/images/space-kat.jpg" 
         />
+        <p className="space-cat-text">Me be fuzzy cat<br/>looking at the pretty lights</p>
       </div>
     </section>
   )
